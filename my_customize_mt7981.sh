3bbaba5c0637d3a9ff8b9d32f79631b31d72e2f5
exlulu <35789196+exlulu@users.noreply.github.com>
2023-12-12 22:38:27 +0800

    fix my_customize_mt7981.sh

--- my_customize_mt7981.sh
+++ my_customize_mt7981.sh
@@ -10,6 +10,3 @@ sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/emortal/default-settings/files/99-default-settings
-
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
