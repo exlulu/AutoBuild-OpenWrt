6e94403d90060be5ceae5829c41c64d996b4107b
exlulu <35789196+exlulu@users.noreply.github.com>
2024-01-02 23:59:06 +0800

    Update my_customize_360_T7.sh

--- my_customize_360_T7.sh
+++ my_customize_360_T7.sh
@@ -7,3 +7,5 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. Modify disable ipv6 support
+sed -i '159s/default y/default n/g' openwrt/config/Config-build.in
