e71376d586a3a015d3f16963931dd7108b8431d6
exlulu <35789196+exlulu@users.noreply.github.com>
2024-01-10 00:17:15 +0800

    Update my_customize_360_T7.sh

--- my_customize_360_T7.sh
+++ my_customize_360_T7.sh
@@ -11,5 +11,3 @@ sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config
 # sed -i '159s/default y/default n/g' openwrt/config/Config-build.in
 #3. Modify show mtk mhz info
 # sed -i '25s#cpu_freq=""#cpu_freq="$\(mhz | awk -F '\'cpu_MHz=\'' '\'\{printf\(\"%.fMHz\",\$2\)\}\''\)"#g' openwrt/package/emortal/autocore/files/generic/cpuinfo
-#4.
-sed -i '53,56d' openwrt/package/emortal/autocore/files/generic/cpuinfo
