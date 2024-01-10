55007ff93f87ac156cdb30378eda7daeb339d864
exlulu <35789196+exlulu@users.noreply.github.com>
2024-01-10 20:01:47 +0800

    Update my_customize_360_T7.sh

--- my_customize_360_T7.sh
+++ my_customize_360_T7.sh
@@ -10,6 +10,6 @@ sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config
 #2. Modify disable ipv6 support
 # sed -i '159s/default y/default n/g' openwrt/config/Config-build.in
 #3. Modify show mtk mhz info
-#sed -i '25s#cpu_freq=""#cpu_freq="$\(mhz | awk -F '\'cpu_MHz=\'' '\'\{printf\(\"%.fMHz\",\$2\)\}\''\)"#g' openwrt/package/emortal/autocore/files/generic/cpuinfo
-#sed -i '53,54d' openwrt/package/emortal/autocore/files/generic/cpuinfo
-#sed -i '62s/ipq/ipq|mediatek/g' openwrt/package/emortal/autocore/files/generic/cpuinfo
+# sed -i '25s/cpu_freq=""/cpu_freq="$\(mhz | awk -F '\'cpu_MHz=\'' '\'\{printf\(\"%.fMHz\",\$2\)\}\''\)"/g' openwrt/package/emortal/autocore/files/generic/cpuinfo
+# sed -i '53,54d' openwrt/package/emortal/autocore/files/generic/cpuinfo
+# sed -i '62s/ipq/ipq|mediatek/g' openwrt/package/emortal/autocore/files/generic/cpuinfo
