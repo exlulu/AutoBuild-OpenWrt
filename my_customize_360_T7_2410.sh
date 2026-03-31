4d59742ddd65fba1b51ba8717e57765551789eba
exlulu <35789196+exlulu@users.noreply.github.com>
2026-03-31 23:43:55 +0800

    Update my_customize_360_T7_2410.sh

--- my_customize_360_T7_2410.sh
+++ my_customize_360_T7_2410.sh
@@ -7,3 +7,8 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. Modify hide mtk mhz info
+sed -i '25s/cpu_freq="$\(mhz | awk -F '\'cpu_MHz=\'' '\'\{printf\(\"%.fMHz\",\$2\)\}\''\)"/cpu_freq=""/g' openwrt/package/emortal/autocore/files/generic/cpuinfo
+sed -i '52a\"mediatek"/*)' openwrt/package/emortal/autocore/files/generic/cpuinfo
+sed -i '53a\    cpu_temp="" ;;' openwrt/package/emortal/autocore/files/generic/cpuinfo
+sed -i '64s/|mediatek//' openwrt/package/emortal/autocore/files/generic/cpuinfo
